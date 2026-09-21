235c5e2c65de40ae10eee49cf43dc567733924eb
ars1an-cod1 <mehmetarslanvatanwork@gmail.com>
2026-09-21 11:07:43 +0000

    lab 2 is done

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -12,17 +12,41 @@
 */
 
 int sum_to_n(int n) {
-    // TODO: implement sum with a for loop
-    return 0; // placeholder
+ 
+
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    printf(" Please input a number that will be summed");
+sum = sum + (i + 1);
+  }
+  // printf(sum);
+  // return 0;
+  return sum;
 }
 
 int main(void) {
-    int n;
+  int n;
 
-    printf("Enter a positive integer n: ");
+  printf("Enter a positive integer n: ");
+  scanf("%d", &n);
+  while (n < 1)
+
+  {
+    printf("N is too low. Please input a 'n' ");
     scanf("%d", &n);
+  }
 
-    // TODO: validate input, call function, and print result
-
-    return 0;
+  // printf the value returned from the sum_to_n function
+  int result = sum_to_n(n);
+    printf("The sum of integers from 1 to %d is: %d\n", n, result);
 }
+
+/*Use a for loop to compute the sum of integers 1 through n.*/
+
+// n main:
+
+// Ask user for n
+
+// If n < 1, print error message
+
+// Otherwise call sum_to_n and print the result
--- src/lab2_2.c
+++ src/lab2_2.c
@@ -12,9 +12,14 @@
 */
 
 long long factorial(int n) {
+    long long result = 1;
+    for (int i=1; i<= n; i++){
+        result=result* i;
+    }
+    return result;
+ }
     // TODO: compute factorial iteratively
-    return 1; // placeholder
-}
+     // placeholder
 
 int main(void) {
     int n;
@@ -22,7 +27,19 @@ int main(void) {
     printf("Enter a non-negative integer n: ");
     scanf("%d", &n);
 
+    while(n<0){
+
+        printf("The number has to be greater than zero add one more number ");
+        scanf("%d", &n);
+        
+    }
+
+    
+    
+
+    
     // TODO: validate input, call function, print result
+    printf("Your factorial number is: %lld\n " , factorial(n));
 
     return 0;
 }
